e9956989fd92b2c8d3828a3a91d50e355165d84e
Fabian Koerner <fab@tech-kyb.de>
2026-02-19 19:38:57 +0000

    initial template config

---
+++ template/src/plugin.ts
@@ -0,0 +1,10 @@
+import streamDeck from "@elgato/streamdeck";
+
+import { Counter } from "./actions/counter";
+
+// Register all actions before connecting.
+// https://docs.elgato.com/streamdeck/sdk/guides/actions
+streamDeck.actions.registerAction(new Counter());
+
+// Connect to Stream Deck — always call this last.
+streamDeck.connect();
